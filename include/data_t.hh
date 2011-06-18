c24dd4dadea578d1b52cd5f9d3d2a5566a559a78
ltrung <ltrung>
2011-06-18 02:00:39 +0000

    original-log: minerva revision 1.2 ; 2011.06.18.02.00.39 ; ltrung a working commit

--- include/data_t.hh
+++ include/data_t.hh
@@ -13,8 +13,21 @@
 
 class data_t 
 {
-   
-public:
+    // copied from G4ProcessType
+    enum ProcessType {
+        fNotDefined,
+        fTransportation,
+        fElectromagnetic,
+        fOptical,             
+        fHadronic,
+        fPhotolepton_hadron,
+        fDecay,
+        fGeneral,
+        fParameterisation,
+        fUserDefined
+    };
+
+    public:
    // a constructor and a destructor
    data_t();
    virtual ~data_t();
@@ -92,6 +105,40 @@ public:
    Int_t    tptype;
    Int_t    tgen;
 
+        
+        /// When a neutrino is produced, save the complete tracking history, up to
+        /// the primary proton.
+        /// number of trajectories between (and including) the neutrino and the primary proton
+    Int_t ntrajectory;
+    
+        /// Assuming the maximum number of generations is 10, can increase if needed
+    Int_t pdg[10];         // particle pdg code
+    Int_t trackId[10];     // particle trackId 
+    Int_t parentId[10];    // parentId 
+    
+    Double_t startx[10];   // particle x initial position
+    Double_t starty[10];   // particle y initial position
+    Double_t startz[10];   // particle z initial position
+    Double_t stopx[10];    // particle x final position
+    Double_t stopy[10];    // particle y final position
+    Double_t stopz[10];    // particle z final position
+    
+    Double_t startpx[10];  // particle x initial momentum
+    Double_t startpy[10];  // particle y initial momentum
+    Double_t startpz[10];  // particle z initial momentum
+    Double_t stoppx[10];   // particle x final momentum
+    Double_t stoppy[10];   // particle y final momentum
+    Double_t stoppz[10];   // particle z final momentum
+    
+    Double_t pprodpx[10];  // parent x momentum when producing this particle
+    Double_t pprodpy[10];  // parent y momentum when producing this particle
+    Double_t pprodpz[10];  // parent z momentum when producing this particle
+    
+    TString proc[10]; // name of the process that creates this particle
+    
+    TString startVol[10]; // name of the volume where the particle starts
+    TString stopVol[10];  // name of the volume where the particle stops
+        
     //
    //the size of these arrays are arbitray
    //and not descriptive
