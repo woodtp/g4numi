0fb1af15d9017bee4b1fe05ebaf3c6bfd7fd2e05
ltrung <ltrung>
2011-06-18 02:00:02 +0000

    original-log: minerva revision 1.2 ; 2011.06.18.02.00.02 ; ltrung a working commit

--- src/data_t.cc
+++ src/data_t.cc
@@ -4,7 +4,7 @@
 //  ADM, July 2005
 //  This is a class that defines the data_t object that is used to 
 //  store the g4numi output in a root tree.
-// $Id: data_t.cc,v 1.3.4.1 2010/08/19 19:50:54 minervacvs Exp $
+// $Id: data_t.cc,v 1.3.4.2 2011/06/18 02:00:02 ltrung Exp $
 //------------------
 
 #include "data_t.hh"
@@ -82,13 +82,17 @@ void data_t::Clear(const std::string &)
    tptype      = -1;
    tgen        = -1;
 
-
+   for (Int_t index = 0; index < 10; ++index) {
+           startVol[index] = "NotDefined";
+           stopVol[index] = "NotDefined";
+           proc[index] = "NotDefined";
+   }
    //
    //the size of these arrays are arbitray
    //and not descriptive
    //this is bad, get rid of this
    //
-   
+      
    for ( Int_t index=0; index<11; ++index)
    {
       NdxdzNear[index] = -1.e4;
