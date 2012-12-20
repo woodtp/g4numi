a140adf67768c241f38d7d19f00c862c6d4ec09d
laliaga <laliaga>
2012-12-20 02:54:34 +0000

    original-log: minerva revision 1.6 ; 2012.12.20.02.54.34 ; laliaga macro input template for g4na49

--- include/NA49Analysis.hh
+++ include/NA49Analysis.hh
@@ -12,7 +12,6 @@
 #include "G4TrajectoryContainer.hh"
 #include "G4ParticleDefinition.hh"
 #include "G4Element.hh"
-#include "G4Material.hh"
 
 #include <map>
 #include <vector>
@@ -40,10 +39,8 @@ public:
    void FillNtuple(std::vector<TrackInfo_t>);
 
    void WriteNtuple();
-   void WriteHeader();
-   void GetDetConstInfo(std::vector<Double_t> DCinfo,G4Material* mat);
    void GetPrimGenInfo(Double_t enerPrim,G4ParticleDefinition* Part);
-   void GetRunActInfo(Int_t nevt);
+
    static NA49Analysis* getInstance();
    void finish();
    
@@ -51,24 +48,13 @@ public:
 private:
    static NA49Analysis* instance;
    
-   char NtupleFileName[50];
+  char NtupleFileName[50];
    TFile* FileNtuple;  
    TTree* ProdTree;
-   TTree* HeaderTree;
    ProdTuple_t g4Proddata; 
-   //info for header:
-   Double_t inelasticXS;    
-   Double_t elasticXS; 
-   Double_t tickness;
-   Double_t radius;
-   Double_t rho;
-   G4Material* mate;
-   std::string material;
    Double_t enerPrimGen;
-   std::string namePrim;
    G4ParticleDefinition* particle;
-   std::string PartName;
-   Int_t numberEvts;
+
 };
 
 #endif 
