5ca5bb5de2f60c18ddd73063382256848a0d326d
marshalc <marshalc>
2013-03-14 12:59:22 +0000

    original-log: minerva revision 1.6 ; 2013.03.14.12.59.22 ; marshalc No functional change--add a comment instructing users which line needs to be commented out if they want to use a local copy of gean4

--- setup_beamsim.sh
+++ setup_beamsim.sh
@@ -17,30 +17,31 @@ setup_beamsim(){
     shift ${OPTIND-1}
     
     if [ "$HOSTNAME" == "if01.fnal.gov" ]; then
-	echo 'Setting up MINERVA batch submission using minerva_jobsub'
-	source /grid/fermiapp/minerva/condor/setup.minerva.condor.sh
-	echo ''
+        echo 'Setting up MINERVA batch submission using minerva_jobsub'
+        source /grid/fermiapp/minerva/condor/setup.minerva.condor.sh
+        echo ''
     fi
         setup -f Linux+2.6-2.5 gcc v3_4_3
-	setup -q g77-OpenGL -f Linux+2.6 geant4 v4_9_2_p03
-	setup -f Linux+2.6 -q GCC_3_4_6 root v5_22_00j
-	setup -f Linux+2.6-2.5 clhep v2_0_4_5
-	export BEAMSIM="${TOP}"
-	export G4NUMIVER="v4"
-# 	setup python # sets up the local version....
-	setup g4photon
-	export LD_LIBRARY_PATH="${LD_LIBRARY_PATH}:$G4LIB/plists/Linux-g++"
+        # Comment OUT the line below if you are using a local geant4 - g4numi_environment.sh will do this correctly for you
+      	setup -q g77-OpenGL -f Linux+2.6 geant4 v4_9_2_p03
+        setup -f Linux+2.6 -q GCC_3_4_6 root v5_22_00j
+        setup -f Linux+2.6-2.5 clhep v2_0_4_5
+        export BEAMSIM="${TOP}"
+        export G4NUMIVER="v4"
+        setup g4photon
+        export LD_LIBRARY_PATH="${LD_LIBRARY_PATH}:$G4LIB/plists/Linux-g++"
 
 # people who just want to run g4numi probably do not 
 # need to set G4WORKDIR
     if (( SETWDIR==1 )); then
-	export G4WORKDIR="${TOP}"
-	echo "G4WORKDIR is ${G4WORKDIR}"
+        export G4WORKDIR="${TOP}"
+        echo "G4WORKDIR is ${G4WORKDIR}"
     else
-	unset G4WORKDIR
+        unset G4WORKDIR
     fi
     
     unset OPTSTRING
     unset OPTIND
 }
 setup_beamsim -w 1
+
