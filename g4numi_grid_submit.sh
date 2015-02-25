68d4b83d32c8822e9901e22ffe8a7c95182c74a2
drut1186 <drut1186>
2015-02-25 20:32:33 +0000

    Move from jobsub to jobsub_client submission methods to avoid issues with shutdown of gpsn01

--- g4numi_grid_submit.sh
+++ g4numi_grid_submit.sh
@@ -1,16 +1,20 @@
 #!/bin/bash
 
-# $Header: /cvs/projects/numi-beam-sim/numi-beam-sim/g4numi/Attic/g4numi_grid_submit.sh,v 1.1.2.5 2014/10/07 16:25:19 kordosky Exp $
+# $Header: /cvs/projects/numi-beam-sim/numi-beam-sim/g4numi/Attic/g4numi_grid_submit.sh,v 1.1.2.6 2015/02/25 20:32:33 drut1186 Exp $
 
 # for interactive running use GRID=""
 #export GRID=""
 # for production account, non group-writable, use GRID="-g"
 #export GRID="-g"
 # for submitting as myself, use GRID="-g --use_gftp"
-export GRID="--environment=LD_LIBRARY_PATH --OS=SL6 -g --opportunistic --use_gftp"
+#export for the old job submission method kept here just in case
+#export GRID="--environment=LD_LIBRARY_PATH --OS=SL6 -g --opportunistic --use_gftp "
+export GRID="--environment=LD_LIBRARY_PATH --OS=SL6 -g --resource-provides=usage_model=DEDICATED,OPPORTUNISTIC --use_gftp --role=Analysis -G minerva "
+
 
 ########################### g4numi configuration ###########################
 export BEAMCONFIG="le010z185i"
+export PLAYLIST="minerva13"
 export DOWATER="false"
 export WATERCM="3"
 export DOIMPWT="true"
@@ -28,5 +32,7 @@ echo "Will use code and scripts from $G4NUMIAREA"
 
 ############################### submit the job #############################
 
-jobsub $GRID -N $NJOBS -dG4NUMI $OUTDIR -e G4NUMIAREA  -e BEAMCONFIG -e DOWATER -e WATERCM -e POT -e RUN -e DOIMPWT -L $LOGFILE $BEAMSIM/g4numi_job.sh
+#old submission method kept here in case 
+#jobsub $GRID -N $NJOBS -dG4NUMI $OUTDIR -e G4NUMIAREA  -e BEAMCONFIG -e DOWATER -e WATERCM -e POT -e RUN -e DOIMPWT -e PLAYLIST -L $LOGFILE $BEAMSIM/g4numi_job.sh
 
+jobsub_submit $GRID -N $NJOBS -dG4NUMI $OUTDIR -e G4NUMIAREA  -e BEAMCONFIG -e DOWATER -e WATERCM -e POT -e RUN -e DOIMPWT -e PLAYLIST -L $LOGFILE file://$BEAMSIM/g4numi_job.sh
