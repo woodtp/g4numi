24bd14bf69533a74c784a8d5dcfc2f16daf0e05a
laliaga <laliaga>
2015-02-25 01:31:31 +0000

    Changing to include dk2nu needs for the file name

--- renameG4Numi.py
+++ renameG4Numi.py
@@ -9,31 +9,35 @@ import os, re, shutil
 # a glob of files to use in GENIE.
 ###########
 # example:
-#    OLD: /minerva/data/flux/g4numi/v4_test/le100z-200i/g4numiv4_le100z-200i_98_0001.root
-#    NEW: /minerva/data/flux/g4numi/v4_test/le100z-200i/g4numiv4_le100z-200i_0098_0001.root
+#    OLD: /minerva/data/flux/g4numi/v5/le100z-200i/g4numiv5_minerva1_le010z185i_0111_0005.root
+#    NEW: /minerva/data/flux/g4numi/v5/le100z-200i/g4numiv5_dk2nu_minerva1_le010z185i_0111_0005.root
 ###########
 # Brian Tice (tice@physics.rutgers.edu)
 # Aug 17, 2012
 #######################################
+# This script was slightly modifed to add dk2nu new format name
+#(Leo Aliaga, Feb 2015)
+#######################################
 
-####################################
-# Params
-####################################
-test    = True   #if true don't move, just say what you would do
-verbose = True   #print the old and new filenames
 
-g4numiv = "v4"   #what is the g4numiv
-fluxdir = "/minerva/data/flux_hadron_samples/flux/g4numi/v4_test"  #what is the top directory for the g4numi files
+
+test    = True
+verbose = True  
+
+g4numiv = "v5" 
+fluxdir = "/minerva/data/users/laliaga/flux/le100z200i"
+
 ####################################
 
 for root, dirs, files in os.walk( os.path.join( fluxdir ) ):
   for name in files:
-    m = re.search( r'g4numi%s_(\S+)_(\d+)_(\d{4}).root' % g4numiv, name )
+    m = re.search( r'g4numi%s_(\S+)_(\S+)_(\d+)_(\d{4}).root' % g4numiv, name )
     if m:
-      beam   = m.group(1)
-      run    = int( m.group(2) )
-      subrun = int( m.group(3) )
-      newname = "g4numi%s_%s_%04d_%04d.root" % (g4numiv, beam, run, subrun )
+      pl     = m.group(1)
+      beam   = m.group(2)
+      run    = int( m.group(3) )
+      subrun = int( m.group(4) )
+      newname = "g4numi%s_dk2nu_%s_%s_%04d_%04d.root" % (g4numiv, pl , beam, run, subrun )
 
       oldfile = os.path.join( root, name )
       newfile = os.path.join( root, newname )
@@ -50,3 +54,5 @@ for root, dirs, files in os.walk( os.path.join( fluxdir ) ):
         shutil.move( oldfile, newfile )
       except Exception, e:
         print "  Problem with move : " + str(e)
+
+        
