91f2e5f24c098674b23056b8fe9c551103993975
lebrun <lebrun>
2017-11-02 21:45:38 +0000

    just adding a call to print the small ASCII histograms

--- src/NumiRunAction.cc
+++ src/NumiRunAction.cc
@@ -184,6 +184,8 @@ void NumiRunAction::EndOfRunAction(const G4Run* aRun)
   CLHEP::HepRandom::saveEngineStatus(randomFile);
   G4cout << "  Random engine status at the end of the run saved in "<<randomFile<<G4endl;
   NumiAnalysis* analysis = NumiAnalysis::getInstance();
+  analysis->finishPL();
+  // We don't care about closing root correctly.. Sometimes, it gets corrupted..Well, study this.. 
   analysis->FillMeta();
   analysis->finish();
 
