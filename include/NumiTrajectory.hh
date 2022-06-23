4f29bb9f7d0884f1853a011f451e8c6f3d5bd4ad
Robert Hatcher <rhatcher@fnal.gov>
2022-06-23 11:45:55 -0500

    carry over functionality for avoiding USEMODGEANT4 from g4lbne

--- include/NumiTrajectory.hh
+++ include/NumiTrajectory.hh
@@ -27,6 +27,7 @@ class NumiDataInput;
 typedef std::vector<G4VTrajectoryPoint*> NumiTrajectoryPointContainer;
 typedef std::vector<G4ThreeVector> NumiTrajectoryMomentumContainer;
 typedef std::vector<G4String> NumiTrajectoryVolumeName;
+typedef std::vector<G4String> NumiTrajectoryMaterialName;
 typedef std::vector<G4double> DVec;
 
 class NumiTrajectory : public G4VTrajectory
@@ -75,25 +76,56 @@ class NumiTrajectory : public G4VTrajectory
    virtual G4double GetStepLength(G4int i) const
       {return (*fStepLength)[i];}
 
-    const G4String& GetProcessName() const { return fProcessName;}
-    void SetProcessName(const G4String& name) { fProcessName = name;}
+   inline G4int GetMaterialNumber1rst() const
+   {return fMaterialNumber1rst;}
+   inline void SetMaterialNumber1rst(G4double matNum)
+   {fMaterialNumber1rst = matNum;}
 
-    inline const G4ThreeVector& GetParentMomentumAtThisProduction() const {
-        return fParentMomentumAtThisProduction;
-    }
+   inline G4int GetMaterialNumberLast() const
+   {return fMaterialNumberLast;}
+   inline void SetMaterialNumberLast(G4double matNum)
+    {fMaterialNumberLast = matNum;}
+
+   inline G4String GetVolName1rst() const
+   {return fVolName1rst;}
+
+   inline G4double GetTimeStart() const
+   {return fTimeStart;}
+
+   inline G4ThreeVector GetPolarization() const
+   { return fPolarization;}
+
+   inline G4int GetPDGNucleus() const {
+    return fPDGNucleus; }
+
+   inline G4String  GetProcessName() const
+   { return fProcessName; }
+
+   inline G4String GetMaterialName1rst() const
+   { return fMaterialName1rst; }
+   virtual G4String GetMaterialName(G4int i)const
+   {return (*fMaterialName)[i];}
+
+   inline const G4ThreeVector& GetParentMomentumAtThisProduction() const {
+     return fParentMomentumAtThisProduction;
+   }
+
+   virtual void ShowTrajectory() const;
+   virtual void ShowTrajectory(std::ostream& o) const;
+   virtual void DrawTrajectory(G4int i_mode=0) const;
+
+   virtual const std::map<G4String,G4AttDef>* GetAttDefs() const;
+   virtual std::vector<G4AttValue>* CreateAttValues() const;
+
+   virtual void AppendStep(const G4Step* aStep);
+   virtual void MergeTrajectory(G4VTrajectory* secondTrajectory);
 
     inline void SetParentMomentumAtThisProduction(const G4ThreeVector& mom) {
         fParentMomentumAtThisProduction = mom;
     }
-  //for dk2nu:
-  inline G4double GetTime() const
-   { return fTime; }
   
-   virtual void ShowTrajectory() const;
-   virtual void ShowTrajectory(std::ostream& o) const;
-   virtual void DrawTrajectory(G4int i_mode=0) const;
-   virtual void AppendStep(const G4Step* aStep);
-   virtual void MergeTrajectory(G4VTrajectory* secondTrajectory);
+   void SetProcessName(const G4String& name) { fProcessName = name;}
+
 
    G4ParticleDefinition* GetParticleDefinition();
 
@@ -115,11 +147,22 @@ class NumiTrajectory : public G4VTrajectory
    G4double                         fParticleMass;
    NumiTrajectoryVolumeName*        fPreStepVolume;
    DVec*                            fStepLength;
+
+   G4int                            fMaterialNumber1rst; // at the first point
+   G4String                         fVolName1rst; // at the first point
+   G4int                            fMaterialNumberLast;  // at the last recorded point
+   G4double                         fTimeStart;
+   G4ThreeVector                    fPolarization;
+   G4int                            fPDGNucleus; // The nucleus that caused the scatter..
+                                                 // if I can find it..
    G4String                         fProcessName;
   /// Momentum of the parent particle when producing this particle
    G4ThreeVector      fParentMomentumAtThisProduction; 
+  NumiTrajectoryMaterialName*       fMaterialName;
+  G4String                          fMaterialName1rst; // first material name where the corresponding track
+                                                       // has been created
   //New for dk2nu:
-  G4double                         fTime;
+  //G4double                         fTime;
     
 
    NumiDataInput* fND;
--- src/NumiTrajectory.cc
+++ src/NumiTrajectory.cc
@@ -10,35 +10,55 @@
 #include "G4Circle.hh"
 #include "G4Colour.hh"
 #include "G4VisAttributes.hh"
+#include "G4AttDefStore.hh"
+#include "G4AttDef.hh"
+#include "G4AttValue.hh"
+#include "G4UIcommand.hh"
+#include "G4VisAttributes.hh"
 #include "G4VVisManager.hh"
 #include "G4UnitsTable.hh"
 #include "G4VProcess.hh"
 #include "NumiTrackInformation.hh"
 #include "NumiDataInput.hh"
 
+
+#include "G4ProcessType.hh"
+#include "G4HadronicProcess.hh"
+// for "EM" nuclear interactions, as "muonNuclear" Process occurs.
+#include "G4MuonNuclearProcess.hh"
+#include "G4ElectronNuclearProcess.hh"
+#include "G4PositronNuclearProcess.hh"
+
 G4Allocator<NumiTrajectory> myTrajectoryAllocator;
 
 NumiTrajectory::NumiTrajectory()
 {
    fParticleDefinition = 0;
-   fParticleName = "";
-   fPDGCharge = 0;
-   fPDGEncoding = 0;
-   fTrackID = 0;
-   fParentID = 0;
-   fPositionRecord = 0;
-   fMomentum = G4ThreeVector(0.,0.,0.);
-   fMomentumRecord = 0;
-   fVertexPosition = G4ThreeVector(0.,0.,0.);
-   fParticleMass = 0.;
-   fDecayCode=0;
-   fTgen=0;
-   fPreStepVolume=0;
-   fStepLength = 0;
-   fProcessName = "";
-   //for dk2nu:
-   fTime = 0.0;
-   //
+   fParticleName       = "";
+   fPDGCharge          = 0;
+   fPDGEncoding        = 0;
+   fTrackID            = 0;
+   fParentID           = 0;
+   fPositionRecord     = 0;
+   fMomentum           = G4ThreeVector(0.,0.,0.);
+   fMomentumRecord     = 0;
+   fVertexPosition     = G4ThreeVector(0.,0.,0.);
+   fParticleMass       = 0.;
+   fDecayCode          = 0;
+   fTgen               = 0;
+   fNImpWt             = 1.;
+   fPreStepVolume      = 0;
+   fStepLength         = 0;
+   fMaterialNumber1rst = 0;
+   fVolName1rst        = std::string("");
+   fMaterialNumberLast = 0;
+   fTimeStart          = 0.;
+   fPolarization       = G4ThreeVector(0.,0.,0.);
+   fParentMomentumAtThisProduction = G4ThreeVector(0.,0.,0.); // for ppfx....A. Bashyal
+   fPDGNucleus         = 0;
+   fProcessName        = std::string("");
+   fMaterialName1rst    = std::string("");
+
    fND=NumiDataInput::GetNumiDataInput();
 
 }
@@ -46,43 +66,104 @@ NumiTrajectory::NumiTrajectory()
 NumiTrajectory::NumiTrajectory(const G4Track* aTrack)
 {
    fParticleDefinition = aTrack->GetDefinition();
-   fParticleName = fParticleDefinition->GetParticleName();
-   fPDGCharge = fParticleDefinition->GetPDGCharge();
-   fPDGEncoding = fParticleDefinition->GetPDGEncoding();
-   fTrackID = aTrack->GetTrackID();
-   fParentID = aTrack->GetParentID();
-   fPositionRecord = new NumiTrajectoryPointContainer();
+   fParticleName       = fParticleDefinition->GetParticleName();
+   fPDGCharge          = fParticleDefinition->GetPDGCharge();
+   fPDGEncoding        = fParticleDefinition->GetPDGEncoding();
+   fTrackID            = aTrack->GetTrackID();
+   fParentID           = aTrack->GetParentID();
+   fPositionRecord     = new NumiTrajectoryPointContainer();
    fPositionRecord->push_back(new G4TrajectoryPoint(aTrack->GetPosition()));
-   fMomentumRecord = new NumiTrajectoryMomentumContainer();
+   fMomentumRecord     = new NumiTrajectoryMomentumContainer();
    fMomentumRecord->push_back(aTrack->GetMomentum());
-   fPreStepVolume = new NumiTrajectoryVolumeName();  
+   fPreStepVolume      = new NumiTrajectoryVolumeName();
    fPreStepVolume->push_back(aTrack->GetVolume()->GetName());
-   fStepLength = new DVec();  
+   fMaterialName       = new NumiTrajectoryMaterialName();
+   fMaterialName->push_back(aTrack->GetVolume()->GetLogicalVolume()->GetMaterial()->GetName());
+   fStepLength         = new DVec();
    fStepLength->push_back(aTrack->GetStepLength());
    fMomentum = aTrack->GetMomentum();
    fVertexPosition = aTrack->GetPosition();
    fParticleMass = aTrack->GetDefinition()->GetPDGMass();
-
+   if (!aTrack->GetLogicalVolumeAtVertex()) {
+//     std::cerr << " NumiTrajectory::NumiTrajectory No logical volume at vertex... ???    " << std::endl;
+     fMaterialNumber1rst = 0;
+     fMaterialName1rst = std::string("Unknown");
+     fVolName1rst = std::string("Unknown");
+    } else {
+     fMaterialNumber1rst = aTrack->GetLogicalVolumeAtVertex()->GetMaterial()->GetIndex();
+     fMaterialName1rst = aTrack->GetLogicalVolumeAtVertex()->GetMaterial()->GetName();
+     fVolName1rst = aTrack->GetLogicalVolumeAtVertex()->GetName();
+   }
    const G4VProcess* pCreatorProcess = aTrack->GetCreatorProcess();
    if (pCreatorProcess) {
        fProcessName = pCreatorProcess->GetProcessName();
    } else {
        fProcessName = "Primary";
    }
-   //for dk2nu:
-   fTime = aTrack->GetGlobalTime();
-   //
-   NumiTrackInformation* info=(NumiTrackInformation*)(aTrack->GetUserInformation());
-   if (info!=0) {
+   fTimeStart = aTrack->GetGlobalTime();
+   fPolarization = aTrack->GetPolarization();
+
+   NumiTrackInformation* info = (NumiTrackInformation*)(aTrack->GetUserInformation());
+   if (info!=0)
+   {
      fDecayCode = info->GetDecayCode();
      fTgen = info->GetTgen();
      fNImpWt = info->GetNImpWt();
      fParentMomentumAtThisProduction = info->GetParentMomentumAtThisProduction();
    }
-   else { 
+   else
+  {
      fDecayCode = 0;
-     fTgen = 0; 
-     fNImpWt = 1.;
+     fTgen      = 0;
+     fNImpWt    = 1.;
+     fParentMomentumAtThisProduction = G4ThreeVector(0.,0.,0.);
+   }
+
+   // Now the tricky bit: Dk2Nu package wants the process name and the nucleus
+   // if (aTrack->GetCreatorProcess() != 0)     fProcessName = aTrack->GetCreatorProcess()->GetProcessName(); // Original 2014 code
+   if (aTrack->GetCreatorProcess() != 0){ // attempt to get based on: https://geant4-forum.web.cern.ch/t/access-information-about-interaction-nucleus-of-material-in-step/5743
+     fProcessName = aTrack->GetCreatorProcess()->GetProcessName();
+
+     // if it derives from G4HadronicProcess then we can ask for the nucleus
+     auto hadronicProcess =
+       dynamic_cast<const G4HadronicProcess*>( aTrack->GetCreatorProcess() );
+
+     if ( hadronicProcess ) {
+       G4Nucleus nucleus = *(hadronicProcess->GetTargetNucleus());
+       fPDGNucleus = 1000000000 +
+         nucleus.GetZ_asInt()*10000 + nucleus.GetA_asInt()*10;
+
+       // flag it as EM process with negative isotope code
+       if ( fProcessName == "muonNuclear"     ||
+            fProcessName == "electronNuclear" ||
+            fProcessName == "positronNuclear"   ) {
+         fPDGNucleus *= -1;
+       }
+     } else {
+       // not derived from G4HadronicProcess
+
+       if (        fProcessName == "Decay"         ) {
+         fPDGNucleus =  1000000000;
+       } else if ( fProcessName == "gamma"         ) {
+         fPDGNucleus = -1000220220;
+       } else if ( fProcessName == "PhotoNuclearXS") {
+         fPDGNucleus = -1000220220;
+       } else if ( fProcessName == "nCapture"      ) {
+         fPDGNucleus =  1000210120;
+       } else if ( fProcessName == "nKiller"       ) {
+         fPDGNucleus = -1000210120;
+       } else {
+         std::cerr << " NumiTrajectory::NumiTrajectory process "
+                   << fProcessName << " not a G4HadronicProcess ... "
+                   << " and not special ProcessName, "
+                   << "can't get nucleus  " << std::endl;
+         fPDGNucleus = -1000000010;
+       }
+     }
+   } else {
+     // no CreatorProcess
+     fProcessName = std::string("BeamParticle");
+     fPDGNucleus = 0;
    }
 
    fND=NumiDataInput::GetNumiDataInput();
@@ -91,16 +172,24 @@ NumiTrajectory::NumiTrajectory(const G4Track* aTrack)
 NumiTrajectory::NumiTrajectory(NumiTrajectory & right)
   : G4VTrajectory()
 {
-  fParticleName = right.fParticleName;
+  fParticleName       = right.fParticleName;
   fParticleDefinition = right.fParticleDefinition;
-  fPDGCharge = right.fPDGCharge;
-  fPDGEncoding = right.fPDGEncoding;
-  fTrackID = right.fTrackID;
-  fParentID = right.fParentID;
-  fPositionRecord = new NumiTrajectoryPointContainer();
-  fMomentumRecord = new NumiTrajectoryMomentumContainer();
-  fPreStepVolume  = new NumiTrajectoryVolumeName();  
-  fStepLength     = new DVec();  
+  fPDGCharge          = right.fPDGCharge;
+  fPDGEncoding        = right.fPDGEncoding;
+  fTrackID            = right.fTrackID;
+  fParentID           = right.fParentID;
+  fMomentum           = right.fMomentum;
+  fVertexPosition     = right.fVertexPosition;
+  fParticleMass       = right.fParticleMass;
+  fDecayCode          = right.fDecayCode;
+  fTgen               = right.fTgen;
+  fNImpWt             = right.fNImpWt;
+  fPositionRecord     = new NumiTrajectoryPointContainer();
+  fMomentumRecord     = new NumiTrajectoryMomentumContainer();
+  fPreStepVolume      = new NumiTrajectoryVolumeName();
+  fStepLength         = new DVec();
+  fParentMomentumAtThisProduction = right.fParentMomentumAtThisProduction;
+  fMaterialName       = new NumiTrajectoryMaterialName();
 
   for(size_t i=0;i<right.fPositionRecord->size();i++)
     {
@@ -122,14 +211,30 @@ NumiTrajectory::NumiTrajectory(NumiTrajectory & right)
       G4double rightsteplength =(G4double)((*(right.fStepLength))[i]);
       fStepLength->push_back(rightsteplength);
    }
-  fMomentum = right.fMomentum;
-  fVertexPosition = right.fVertexPosition;
-  fParticleMass = right.fParticleMass;
-  fDecayCode = right.fDecayCode;
-  fTgen = right.fTgen;
-  fNImpWt = right.fNImpWt;
-  //for dk2nu:
-  fTime = right.fTime;
+  for(size_t i=0;i<right.fMaterialName->size();i++)
+    {
+      G4String rightMaterialName = (G4String)((*(right.fMaterialName))[i]);
+      fMaterialName->push_back(rightMaterialName);
+    }
+
+
+  fMaterialNumber1rst = right.fMaterialNumber1rst;
+  fVolName1rst        = right.fVolName1rst;
+  fMaterialNumberLast = right.fMaterialNumberLast;
+  fTimeStart          = right.fTimeStart;
+  fPolarization       = right.fPolarization;
+  fPDGNucleus         = right.fPDGNucleus;
+  fProcessName        = right.fProcessName;
+  fMaterialName1rst   = right.fMaterialName1rst;
+  fMaterialName       = right.fMaterialName;
+  fMomentum           = right.fMomentum;
+  fVertexPosition     = right.fVertexPosition;
+  fParticleMass       = right.fParticleMass;
+  fDecayCode          = right.fDecayCode;
+  fTgen               = right.fTgen;
+  fNImpWt             = right.fNImpWt;
+
+
   //
   fND=NumiDataInput::GetNumiDataInput();
 }
@@ -145,32 +250,35 @@ NumiTrajectory::~NumiTrajectory()
   delete fPositionRecord;
 
   fMomentumRecord->clear();
-  
+
   delete fMomentumRecord;
   fPreStepVolume->clear();
   delete fPreStepVolume;
 
   fStepLength->clear();
   delete fStepLength;
- 
+
+  fMaterialName->clear();
+  delete fMaterialName;
+
 }
 
 
 void NumiTrajectory::ShowTrajectory() const
 {
-   G4cout << G4endl << "TrackID =" << fTrackID 
+   G4cout << G4endl << "TrackID =" << fTrackID
         << " : ParentID=" << fParentID << G4endl;
-   G4cout << "Particle name : " << fParticleName 
+   G4cout << "Particle name : " << fParticleName
         << "  Charge : " << fPDGCharge << G4endl;
    G4cout << "Original momentum : " <<
 G4BestUnit(fMomentum,"Energy") << G4endl;
    G4cout << "Vertex : " << G4BestUnit(fVertexPosition,"Length") << G4endl;
-   G4cout << "  Current trajectory has " << fPositionRecord->size() 
+   G4cout << "  Current trajectory has " << fPositionRecord->size()
         << " points." << G4endl;
 
    for( size_t i=0 ; i < fPositionRecord->size() ; i++){
        G4TrajectoryPoint* aTrajectoryPoint = (G4TrajectoryPoint*)((*fPositionRecord)[i]);
-       G4cout << "Point[" << i << "]" 
+       G4cout << "Point[" << i << "]"
             << " Position= " << aTrajectoryPoint->GetPosition() << G4endl;
    }
 }
@@ -194,6 +302,36 @@ void NumiTrajectory::DrawTrajectory(G4int /*i_mode*/) const
      pPolyline.push_back( pos );
    }
 
+
+   G4Colour color(1.0,1.0,1.0); //white
+
+#ifdef SCHEME_LBNE
+   if(fPDGCharge ==  0.)   color = G4Colour::White();
+   if(fPDGCharge ==  1.0)  color = G4Colour(1.0, 0.5, 0.0); //orange
+   if(fPDGCharge == -1.0)  color = G4Colour(0.5, 0.0, 0.5); //purple
+
+
+   if (fParticleDefinition==G4Proton::ProtonDefinition()) color = G4Color::Green();
+   if (fParticleDefinition==G4Neutron::NeutronDefinition()) color = G4Color::Grey();
+   if (fParticleDefinition==G4PionMinus::PionMinusDefinition()) color = G4Color::Cyan();
+   if (fParticleDefinition==G4PionPlus::PionPlusDefinition()) color = G4Color::Blue();
+   if (fParticleDefinition==G4KaonMinus::KaonMinusDefinition()) color = G4Color::Yellow();
+   if (fParticleDefinition==G4KaonPlus::KaonPlusDefinition()) color = G4Color::Yellow();
+   if (fParticleDefinition==G4MuonMinus::MuonMinusDefinition()) color = G4Color::Red();
+   if (fParticleDefinition==G4MuonPlus::MuonPlusDefinition()) color = G4Color::Red();
+
+   if ((fParticleDefinition == G4NeutrinoE::NeutrinoEDefinition())  ||
+       (fParticleDefinition == G4NeutrinoMu::NeutrinoMuDefinition()) ||
+       (fParticleDefinition == G4NeutrinoTau::NeutrinoTauDefinition()) ||
+       (fParticleDefinition == G4AntiNeutrinoE::AntiNeutrinoEDefinition()) ||
+       (fParticleDefinition == G4AntiNeutrinoMu::AntiNeutrinoMuDefinition()) ||
+       (fParticleDefinition == G4AntiNeutrinoTau::AntiNeutrinoTauDefinition()))
+      color = G4Color::Magenta();
+
+   G4VisAttributes attribs(color);
+   pPolyline.SetVisAttributes(attribs);
+   if(pVVisManager) pVVisManager->Draw(pPolyline);
+#else
    G4Colour colour(0.2,0.2,0.2);
    if(fParticleDefinition==G4Gamma::GammaDefinition())
       colour = G4Colour(0.,0.,1.);
@@ -268,33 +406,115 @@ void NumiTrajectory::DrawTrajectory(G4int /*i_mode*/) const
          if(pVVisManager) pVVisManager->Draw(pPolyline);
       }
    }
-   
+
    //pPolyline.SetVisAttributes(attribs);
    //if(pVVisManager) pVVisManager->Draw(pPolyline);
+#endif
 }
+
+//-------------------------------------------------------------------------
+const std::map<G4String,G4AttDef>* NumiTrajectory::GetAttDefs() const
+{
+    G4bool isNew;
+    std::map<G4String,G4AttDef>* store
+       = G4AttDefStore::GetInstance("NumiTrajectory",isNew);
+    if (isNew)
+    {
+
+       G4String ID("TrkID");
+       (*store)[ID] = G4AttDef(ID,"Track ID","Physics","","G4int");
+
+       G4String PID("ParTrkID");
+       (*store)[PID] = G4AttDef(PID,"Parent ID","Physics","","G4int");
+
+       G4String PN("Name");
+       (*store)[PN] = G4AttDef(PN,"Particle Name","Physics","","G4String");
+
+       G4String Ch("Q");
+       (*store)[Ch] = G4AttDef(Ch,"Charge","Physics","e+","G4double");
+
+       G4String PDG("PDG");
+       (*store)[PDG] = G4AttDef(PDG,"PDG Encoding","Physics","","G4int");
+
+       G4String IMom("IMom");
+       (*store)[IMom] = G4AttDef(IMom, "Momentum at start of trajectory",
+                                 "Physics","G4BestUnit","G4ThreeVector");
+
+       G4String IMag("IMomMag");
+       (*store)[IMag] =
+         G4AttDef(IMag, "Magnitude of momentum at start of trajectory",
+                  "Physics","G4BestUnit","G4double");
+
+       G4String NTP("NPts");
+       (*store)[NTP] = G4AttDef(NTP,"No. of points","Physics","","G4int");
+
+    }
+    return store;
+}
+//-------------------------------------------------------------------------
+std::vector<G4AttValue>* NumiTrajectory::CreateAttValues() const
+{
+   std::vector<G4AttValue>* values = new std::vector<G4AttValue>;
+
+   values->push_back
+      (G4AttValue("TrkID",G4UIcommand::ConvertToString(fTrackID),""));
+
+   values->push_back
+      (G4AttValue("ParTrkID",G4UIcommand::ConvertToString(fParentID),""));
+
+   values->push_back(G4AttValue("Name",fParticleName,""));
+
+   values->push_back
+      (G4AttValue("Q",G4UIcommand::ConvertToString(fPDGCharge),""));
+
+   values->push_back
+      (G4AttValue("PDG",G4UIcommand::ConvertToString(fPDGEncoding),""));
+
+   values->push_back
+      (G4AttValue("IMom",G4BestUnit(GetInitialMomentum(),"Energy"),""));
+
+   values->push_back
+      (G4AttValue("IMomMag",G4BestUnit((GetInitialMomentum()).mag(),"Energy"),""));
+
+   values->push_back
+      (G4AttValue("NPts",G4UIcommand::ConvertToString(GetPointEntries()),""));
+
+   return values;
+}
+
+
+//-------------------------------------------------------------------------
 
 void NumiTrajectory::AppendStep(const G4Step* aStep)
 {
   fPositionRecord
     ->push_back(new G4TrajectoryPoint(aStep->GetPostStepPoint()
-				      ->GetPosition() ));
+                                      ->GetPosition() ));
    fMomentumRecord->push_back(aStep->GetPostStepPoint()->GetMomentum());
 
    G4Track* aTrack=aStep->GetTrack();
    NumiTrackInformation* info=(NumiTrackInformation*)(aTrack->GetUserInformation());
    if (info!=0) {
      fDecayCode=info->GetDecayCode();
+     fParentMomentumAtThisProduction = info->GetParentMomentumAtThisProduction();
      fTgen=info->GetTgen();
    }
-   else fDecayCode=-1;
-   
-   G4StepPoint * steppoint=aStep->GetPreStepPoint(); 
-   G4String PreVolumeName=steppoint->GetPhysicalVolume()->GetName(); 
-   fPreStepVolume->push_back(PreVolumeName); 
-   fStepLength->push_back(aStep->GetStepLength());
+   else
+   {
+     fDecayCode=-1;
+     fParentMomentumAtThisProduction = G4ThreeVector(0.,0.,0.);
+   }
 
+
+   G4StepPoint * steppoint=aStep->GetPreStepPoint();
+   G4String PreVolumeName=steppoint->GetPhysicalVolume()->GetName();
+   G4String MaterialName =
+     steppoint->GetPhysicalVolume()->GetLogicalVolume()->GetMaterial()->GetName();
+   fPreStepVolume->push_back(PreVolumeName);
+   fStepLength->push_back(aStep->GetStepLength());
+   fMaterialName->push_back(MaterialName);
 }
-  
+
 G4ParticleDefinition* NumiTrajectory::GetParticleDefinition()
 {
    return (G4ParticleTable::GetParticleTable()->FindParticle(fParticleName));
@@ -314,4 +534,3 @@ void NumiTrajectory::MergeTrajectory(G4VTrajectory* secondTrajectory)
   seco->fPositionRecord->clear();
 
 }
-
